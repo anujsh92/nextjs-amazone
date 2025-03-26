6293c9f5e1f63a5e92a6b02b9433fe7d29e94d71
anujsh92 <anuj1992sharma@outlook.de>
2025-03-26 18:56:24 +0100

    update files changes

---
+++ lib/constants.ts
@@ -0,0 +1,7 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Nxt Aamzone'
+export const SENDER_NAME = process.env.SENDER_NAME || 'support'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+
+export const USER_ROLES = ['Admin', 'User']
+export const COLORS = ['Gold', 'Green', 'Red']
+export const THEMES = ['Light', 'Dark', 'System']
